e1e9a52a7d91fce2b1cfd54904440906a3fb7b73
unknown <kootlook.applications@gmail.com>
2021-10-07 19:24:18 +0600

    initial commit

--- constants_list_bot.py
+++ constants_list_bot.py
@@ -1,6 +1,47 @@
-import pandas as pd
-import numpy as np
+from constantsdb import ConstantsDB
+from flask import Flask, request
+import os
+import telebot
 
-constants_df   = pd.read_csv('.\\data\\constants.csv', skiprows=5)
+TOKEN   = os.environ.get('API_TOKEN', '')
+APP_URL = f"{os.environ.get('HEROKU_APP_URL', '')}/{TOKEN}"
 
-print(constants_df.head())
+bot     = telebot.TeleBot(TOKEN)
+server  = Flask(__name__)
+db      = ConstantsDB()
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo(message):
+
+    const = db.get_item(message.text)
+    if const is not None:
+        bot.reply_to(message, f"{const['name']} : {const['value']} {const['unit']}")
+    else:
+        bot.reply_to(message, f"Sorry, no constant was found for: {message.text}")
+
+@server.route('/' + TOKEN, methods=['POST'])
+def get_message():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return '!', 200
+
+
+@server.route('/')
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_URL)
+    return '!', 200
+
+
+if __name__ == '__main__':
+    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
+    
+#print(db.get_children('classical_mech'))
+#print(db.get_item('grav_const_SI'))
